9834bd897b55987cd19120447fe8c90fc1a4e889
Vanessa <vanessarouhier0@gmail.com>
2023-07-07 11:24:57 +0200

    nex

--- interface.py
+++ interface.py
@@ -1,29 +1,24 @@
-# import if libraries
+# import libraries
 from tkinter import *
 from PIL import ImageTk, Image
 import csv
-import pandas as pd
 import io
-from tkhtmlview import HTMLLabel
-import numpy as np
-import seaborn as sns
-from tkinter import ttk
 import tkinter.font as tkFont
-
 import importlib
-import wind2_bis,wind1_bis
+import window0
+from tkinter import filedialog
+importlib.reload(window0)
 
+main = Tk()
+main.title("Interface for clinician")
 
-importlib.reload(wind2_bis) #last version of wind2_bis
-importlib.reload(wind1_bis) #last version of wind1_bis
-
-#creation of a window
-root=Tk()
-
-#we give a title for the window, a size by default and a background color also
-root.title("Interface for clinician")
-root.geometry("1200x700")
-root.configure(bg="#CAD0DA")
+# open the window in the middle of my screen 
+window_width = 1200
+window_height = 740
+x= (main.winfo_screenwidth()-window_width)//2
+y = (main.winfo_screenheight()-(window_height+40))//2
+main.geometry(f"{window_width}x{window_height}+{x}+{y}")
+main.configure(bg="#CAD0DA")
 
 #avoid to write it always
 link ='C:\\Users\\User\\Documents\\Stage_4A_Vanessa\\Interface\\Images'
@@ -31,25 +26,57 @@ logo = link + '\\openBCI.ico'
 
 
 #we add a other logo (here that of the openBCI)
-image = root.iconbitmap(logo)
+image = main.iconbitmap(logo)
 
 # We open the image for the home page with our specific size and we load it
 image = Image.open(link + "\\banniere.png")
 image = image.resize((1200, 600))
 my_img = ImageTk.PhotoImage(image)
-my_label = Label(root, image=my_img)
+my_label = Label(main, image=my_img)
 my_label.pack()
 
-
-#next step : we create the button which call window1 when we click 
+# ---------------------------------- for the data file -----------------------------------
+file_path_data = ""
+file_path_event = ""
 bold_font = tkFont.Font(weight="bold")
-b1 = Button(root, text="Click to see the raw data ", command= lambda : wind1_bis.window_1(),font=bold_font)
-b1.place(x=200, y=650)
+def open_file():
+    global file_path_data
+    file_path_data = filedialog.askopenfilename()
+    if file_path_data:
+        label = Label(main, text="Data file path : \n"+file_path_data)
+        label.place(x=315,y=628)
+    else:
+        label = Label(main, text="Error: No data file selected.", fg="red")
+        label.place(x=315, y=628)
+    
+button = Button(main, text="Select data file to open", command=open_file,font=bold_font)
+button.place(x=52,y=625)
+# ----------------------------------------------------------------------------------------
 
-#we create a second button which call window2 when we click 
-b2 = Button(root, text="Click to see the data after the ASR ", command= lambda : wind2_bis.window_2(),font=bold_font)
-# we also place the button where we want
-b2.place(x=710, y=650)
+# ---------------------------------- for the event file ----------------------------------
+def open_file_e():
+    global file_path_event
+    file_path_event = filedialog.askopenfilename()
+    if file_path_event :
+        label = Label(main, text="Event file path : \n"+file_path_event)
+        label.place(x=315,y=693)
+    else:
+        label = Label(main, text="Error: No data file selected.", fg="red")
+        label.place(x=315, y=693)
+        
+button_event = Button(main,text="Select event file to open", command=open_file_e, font=bold_font)
+button_event.place(x=50,y=690)
+# ----------------------------------------------------------------------------------------
 
+# we create a button which call window0 when we click and an error message appear if a file is not selected
+def visualize_data():
+    if file_path_data and file_path_event:
+        window0.windoww0(file_path_data, file_path_event)
+    else:
+        error_label = Label(main, text="Error: Data file or event file is missing.", fg="red")
+        error_label.place(x=870, y=695)
 
-root.mainloop()
+b2 = Button(main, text="Visualize data", command=visualize_data, font=bold_font)
+b2.place(x=900, y=663)
+
+main.mainloop()
